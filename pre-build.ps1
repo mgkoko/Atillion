4c436828b1aa6428bc9e578923fb0789b9cc5fef
Ko Ko <koko@webigence.com>
2015-08-11 12:06:41 +0100

    dun pack removed

--- pre-build.ps1
+++ pre-build.ps1
@@ -24,7 +24,3 @@ else
 Get-ChildItem -Path $PSScriptRoot\src -Filter *.xproj -Recurse | ForEach-Object { 
 	& dnu restore  $_.FullName 2>1 
 }
- # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\src -Filter *.xproj -Recurse | ForEach-Object { 
-	& dnu pack  $_.FullName 2>1 
-}
